d5e77dd04bd0a5ab1b733a39672b655b53c6599d
Tyler Weaver <maybe@tylerjw.dev>
2022-10-27 19:47:13 -0600

    Health Monitor and Cat (#1)
    * Health Monitor and Cat
    
    * Addressed comments for const and checked random
    
    Co-authored-by: Griswald Brooks <griswald.brooks@picknik.ai>

---
+++ cat/cat.ino
@@ -0,0 +1,36 @@
+#include <BLEDevice.h>
+#include <string>
+
+#define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP 5        /* Time ESP32 will go to sleep (in seconds) */
+#define RSSI_LOWER_BOUND -100  // rssi less than this ignored
+#define ALIVE_SECONDS 2
+
+#define NAME "Health Monitor - Zombie -1"
+
+int const led_pin = 2;
+RTC_DATA_ATTR int bootCount = 0;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // Increment boot number and print it every reboot
+    ++bootCount;
+    Serial.println("Boot number: " + String(bootCount));
+
+    // Start bluetooth to advertise our state
+    BLEDevice::init(NAME);
+    BLEDevice::startAdvertising();
+    delay(ALIVE_SECONDS * 1000);
+
+    // Flush the serial buffer and go to sleep
+    Serial.flush();
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+    esp_deep_sleep_start();
+}
+
+void loop()
+{
+    // This is not going to be called
+}
---
+++ health_monitor/health_monitor.ino
@@ -0,0 +1,168 @@
+#include <BLEDevice.h>
+#include <string>
+
+constexpr auto uS_TO_S_FACTOR = 1000000; /* Conversion factor for micro seconds to seconds */
+constexpr auto TIME_TO_SLEEP = 5;        /* Time ESP32 will go to sleep (in seconds) */
+constexpr auto RSSI_LOWER_BOUND = -100;  // rssi less than this ignored
+constexpr auto SCAN_SECONDS = 3;
+
+constexpr auto PREFIX = "Health Monitor - ";
+constexpr auto CAT =    "Zombie -1";
+constexpr auto IMMUNE = "Immune";
+constexpr auto SUPER  = "Super Healthy";
+constexpr auto HEALTY = "Healthy";
+constexpr auto A_SYMPTOMATIC = "Asymptomatic";
+constexpr auto SICK = "Sick";
+constexpr auto ZOMBIE = "Zombie";
+
+constexpr auto led_pin = 2;
+RTC_DATA_ATTR int bootCount = 0;
+RTC_DATA_ATTR unsigned int health = 40;
+
+bool is_monitor(std::string const& name)
+{
+    return name.rfind(PREFIX, 0) == 0;
+}
+
+bool is_close(int rssi)
+{
+    return rssi >= RSSI_LOWER_BOUND;
+}
+
+bool is_infected(std::string const &name)
+{
+    auto const state = name.substr(std::string(PREFIX).length());
+    return state == CAT
+     || state == A_SYMPTOMATIC
+     || state == SICK
+     || state == ZOMBIE;
+}
+
+BLEScanResults scan_ble()
+{
+    auto *scan = BLEDevice::getScan();
+    scan->setActiveScan(true);
+    scan->start(SCAN_SECONDS);
+    delay(1000 * SCAN_SECONDS);
+    scan->stop();
+    return scan->getResults();
+}
+
+unsigned int count_infected(BLEScanResults &results)
+{
+    unsigned int infected = 0;
+    for (auto i = 0; i < results.getCount(); ++i)
+    {
+        auto device = results.getDevice(i);
+        auto const rssi = device.getRSSI();
+        auto const name = device.getName();
+
+        if (is_monitor(name) && 
+            is_close(rssi) && 
+            is_infected(name)) {
+            ++infected;
+        }
+    }
+    return infected;
+}
+
+void print_scan_results(BLEScanResults &results)
+{
+    Serial.println("---<< scan results >>--");
+    for (auto i = 0; i < results.getCount(); ++i)
+    {
+        auto device = results.getDevice(i);
+        auto address = BLEAddress(device.getAddress());
+        auto const rssi = device.getRSSI();
+        auto const name = device.getName();
+
+        Serial.print("Addr: ");
+        Serial.print(address.toString().c_str());
+        Serial.print(", RSSI: ");
+        Serial.print(rssi);
+        Serial.print(", Name: ");
+        Serial.print(name.c_str());
+        Serial.println("");
+    }
+    Serial.println("---");
+}
+
+std::string to_state(unsigned int health) {
+    if (health == 0)
+    {
+        return IMMUNE;
+    }
+    else if (health < 10)
+    {
+        return SUPER;
+    }
+    else if (health < 50)
+    {
+        return HEALTY;
+    }
+    else if (health < 60)
+    {
+        return A_SYMPTOMATIC;
+    }
+    else if (health < 99)
+    {
+        return SICK;
+    }
+    return ZOMBIE;
+}
+
+unsigned int update_health(
+    unsigned int current_health,
+    unsigned int infected)
+{
+    if (health == 0 || health == 100) {
+        return current_health;
+    }
+
+    if (infected > 0) {
+        return current_health + 1;
+    }
+
+    return current_health;
+}
+
+void setup()
+{
+    // start serial
+    Serial.begin(115200);
+
+    // Increment boot number and print it every reboot
+    ++bootCount;
+    Serial.println("Boot number: " + String(bootCount));
+
+    // String representing our state (used by other devices to observe us)
+    auto const state = to_state(health);
+    Serial.println(state.c_str());
+    Serial.println("Health: " + String(health));
+
+    // Start bluetooth to advertise our state
+    BLEDevice::init(PREFIX + state);
+    BLEDevice::startAdvertising();
+
+    // Scan for nearby devices and count infected
+    auto devices = scan_ble();
+    print_scan_results(devices);
+    auto const infected = count_infected(devices);
+    Serial.println("Infected count: " + String(infected));
+
+    // Update our health value
+    health = update_health(health, infected);
+
+    // Flush the serial buffer
+    Serial.flush();
+
+    // Enable waking up in some amount of time and sleep
+    // Tests confirm that random does not produce the same number after reset
+    esp_sleep_enable_timer_wakeup(random(1, TIME_TO_SLEEP) * uS_TO_S_FACTOR);
+    esp_deep_sleep_start();
+}
+
+void loop()
+{
+    // This is not going to be called
+}
