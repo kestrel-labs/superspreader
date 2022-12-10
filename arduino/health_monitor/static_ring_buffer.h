8d88372af266e7630728ef36e163d4a835c07a88
Brian Cairl <briancairl@gmail.com>
2022-12-09 17:08:17 -0800

    Fix behavior of `empty`; thank you AI overlords

--- arduino/health_monitor/static_ring_buffer.h
+++ arduino/health_monitor/static_ring_buffer.h
@@ -2,14 +2,15 @@
 
 #include <cstdint>
 #include <utility>
+#include <stdexcept>
 
-template <typename ValueT, std::size_t N>
+template <typename ValueT, std::size_t N, bool SafeMode = true>
 class static_ring_buffer {
     static_assert(N > 0, "effective size of ring buffer must be > 0");
 
    public:
     static_ring_buffer()
-        : wr_p_{reinterpret_cast<ValueT*>(buffer_)}, rd_p_{reinterpret_cast<ValueT*>(buffer_)} {}
+        : wr_p_{reinterpret_cast<ValueT*>(buffer_)}, rd_p_{reinterpret_cast<ValueT*>(buffer_)}, size_{0} {}
 
     ~static_ring_buffer() {
         while (!empty()) {
@@ -17,12 +18,20 @@ class static_ring_buffer {
         }
     }
 
-    constexpr bool empty() const { return wr_p_ == rd_p_; }
+    constexpr bool empty() const { return size_ == 0; }
+
+    constexpr std::size_t size() const { return size_; }
 
     template <typename... Args>
     void emplace_back(Args&&... args) {
+        if /*constexpr*/ (SafeMode) {
+            if (size_ == N) {
+                throw std::range_error{"adding value would exceed max container capacity"};
+            }
+        }
         new (wr_p_) ValueT{std::forward<Args>(args)...};
         ++wr_p_;
+        ++size_;
         if (wr_p_ == (reinterpret_cast<ValueT*>(buffer_) + N)) {
             wr_p_ = reinterpret_cast<ValueT*>(buffer_);
         }
@@ -31,8 +40,14 @@ class static_ring_buffer {
     constexpr ValueT& front() { return *rd_p_; }
 
     void pop_front() {
+        if /*constexpr*/ (SafeMode) {
+            if (size_ == 0) {
+                throw std::range_error{"container is already empty"};
+            }
+        }
         rd_p_->~ValueT();
         ++rd_p_;
+        --size_;
         if (rd_p_ == (reinterpret_cast<ValueT*>(buffer_) + N)) {
             rd_p_ = reinterpret_cast<ValueT*>(buffer_);
         }
@@ -42,4 +57,5 @@ class static_ring_buffer {
     ValueT* wr_p_;
     ValueT* rd_p_;
     alignas(ValueT) std::uint8_t buffer_[N * sizeof(ValueT)];
+    std::size_t size_;
 };
