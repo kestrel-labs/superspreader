dfdc05a9f935f97e6ebeedb2abb071a280ac343f
Brian Cairl <briancairl@gmail.com>
2022-12-15 22:17:09 -0500

    Adds cmake target and gtests for `health_monitor` code (#29)
    * Adds cmake target and gtests for `health_monitor` code
    
    * --fixup lint
    
    * Update arduino/CMakeLists.txt
    
    yes
    
    Co-authored-by: Tyler Weaver <maybe@tylerjw.dev>
    
    * --fixup no cmake install
    
    * --fixup install prefix
    
    * --fixup cmake.in
    
    * moar
    
    * Decay tests
    
    * --fixup formatting
    
    * --fixup
    
    Co-authored-by: Tyler Weaver <maybe@tylerjw.dev>

---
+++ arduino/cmake/third_party/googletest.cmake
@@ -0,0 +1,13 @@
+project(googletest-download NONE)
+
+include(ExternalProject)
+ExternalProject_Add(googletest
+  GIT_REPOSITORY    https://github.com/google/googletest.git
+  GIT_TAG           c9ccac7cb7345901884aabf5d1a786cfa6e2f397
+  SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-src"
+  BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-build"
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND     ""
+  INSTALL_COMMAND   ""
+  TEST_COMMAND      ""
+)
